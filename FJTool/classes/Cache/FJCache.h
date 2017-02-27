2bc3afc545af42cd357850a1b0fc2a4bae8be122
jeffnjut <jeff_njut@163.com>
2017-02-27 12:11:49 +0800

    add sandbox struture intro

--- FJTool/classes/Cache/FJCache.h
+++ FJTool/classes/Cache/FJCache.h
@@ -8,6 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+// "应用程序包": 这里面存放的是应用程序的源文件，包括资源文件和可执行文件。
+// let path = NSBundle.mainBundle().bundlePath
+
+// Documents: 最常用的目录，iTunes同步该应用时会同步此文件夹中的内容，适合存储重要数据。
+// let documentPaths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)let documentPath = documentPaths[0] as! String或者let documentPath = NSHomeDirectory() + "/Documents"
+
+// Library/Caches: iTunes不会同步此文件夹，适合存储体积大，不需要备份的非重要数据。
+// let cachePaths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.CachesDirectory, NSSearchPathDomainMask.UserDomainMask, true)let CachePath = cachePaths[0] as! String或者let documentPath = NSHomeDirectory() + "/Library"let documentPath = NSHomeDirectory() + "/Library/Caches"
+
+// Library/Preferences: iTunes同步该应用时会同步此文件夹中的内容，通常保存应用的设置信息。
+
+// tmp: iTunes不会同步此文件夹，系统可能在应用没运行时就删除该目录下的文件，所以此目录适合保存应用中的一些临时文件，用完就删除。
+// let tmpPath = NSTemporaryDirectory()
+
 @interface FJCache : NSObject
 
 // 获取沙盒Library的文件目录。
