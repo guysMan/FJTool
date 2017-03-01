df6144ffd9eac23001763f4106bd4519541db4c2
jeffnjut <jeff_njut@163.com>
2017-03-01 10:20:07 +0800

    mod json

--- FJToolExample/AppDelegate.m
+++ FJToolExample/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "FJRegion.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    RegionModel *region = [FJRegion domesticDistricts];
+    NSLog(@"%@",region);
+    
     return YES;
 }
 
